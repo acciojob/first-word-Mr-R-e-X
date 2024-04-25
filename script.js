c44a1f03d8c889a7a1af164916ddd84a595de05e
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-25 12:16:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	  if (s.length === 0) {
+    return "";
+  }
+  s = s.trim();
+  s = s.split(" ");
+  console.log(s);
+  return s[0];
 }
 
-// Do not change the code below
+// Do not change the code below  
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
